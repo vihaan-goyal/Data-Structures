58d5232d39194e713a916e04d8e57f934db87ef7
Vihaan Goyal <vihaan.goyal1512@gmail.com>
2026-08-25 09:51:46 -0400

    Added documentation

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,11 @@
+/**
+ * HelloWorld
+ * @author(V. Goyal)
+ * @version (8/25/26)
+*/
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, world!");
     }
 }
