35f23e2dec688f76211805589afd0fa59b606244
Vihaan Goyal <vihaan.goyal1512@gmail.com>
2026-08-25 15:40:42 -0400

    Add HelloWorld and TaxicabDistance classes with main methods

---
+++ 8-25-26/TaxicabDistance.java
@@ -0,0 +1,26 @@
+/**
+ * TaxicabDistance
+ * @author(V. Goyal)
+ * @version (8/25/26)
+*/
+public class TaxicabDistance {
+    static int startX = 0;
+    static int startY = 0;
+    static int distance = 1;
+
+    
+    public static void main(String[] args){
+
+        for (int i = startX - distance; i <= startX + distance; i++) {
+            for (int j = startY - distance; j <= startY + distance; j++) {
+                if((getDistance(i, j)) <= distance)
+                    System.out.println("(" + i + "," + j + ")");
+            }
+        }
+        
+    }
+
+    public static int getDistance(int x, int y){
+        return Math.abs(x - startX) + Math.abs(y - startY);
+    }
+}
